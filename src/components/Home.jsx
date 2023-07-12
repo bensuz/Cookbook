bbd70eee117fe5b67987d49e7911ae34147afb66
Keerthana Prabhakar <keerthanapillai.kp@gmail.com>
2023-07-12 18:11:18 +0200

    completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,17 +9,17 @@ const Home = ({ recipes }) => {
       <p>This is the home</p>
       <div className="App">
         
-        <Link to={`/recipes/${recipe.sys.id}`}>
         <div>
           {recipes.map((recipe) => (
-          <div key={recipe.sys.id}>
+            <div key={recipe.sys.id}>
+            <Link to={`/recipes/${recipe.sys.id}`}>
               <h3>{recipe.fields.recipe1}</h3>
               {/* <p>{recipe.fields.ingridients}</p> */}
               <img src={recipe.fields.recipe1Media.fields.file.url}></img>
+            </Link>
               </div>
             ))}
             </div>
-            </Link>
         </div>
       
     </>
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import  { useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
+import { createClient } from "contentful";
+import RichText from './RichText';
+
 // import {Link} from 'react-router-dom'
 
 
 // const Recipes = () => {
 //     // const {recipeId} = useParams()
     
-    const Recipes = ({ recipes }) => {
+    const Recipes = () => {
+      const [dish, setDish] = useState(null)
+      const {recipeId} = useParams()
+      const client = createClient({
+        space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+        environment: import.meta.env.VITE_ENVIRONMENT_NAME, // defaults to 'master' if not set
+        accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
+      });
+      useEffect(() => {
+        client
+          .getEntry( recipeId )
+          .then(response => setDish(response))
+
+          .catch(console.error);
+      }, []);
+      if(!dish){
         return (
+          <div>
+          <h2>Serving up your dishes</h2> 
+          </div>
+        )
+      }
+      console.log(dish)
+        return (
+          <div>
+            {dish && 
           <>
-            <p>This is the Recipe</p>
-            <div className="App">
-              <div>
-                {recipes.map((recipe) => (
-                  <div key={recipe.sys.id}>
-                    <h3>{recipe.fields.recipe1}</h3>
-                    <p>{recipe.fields.ingridients}</p>
-                    <img src={recipe.fields.recipe1Media.fields.file.url}></img>
-                    {/* <Link to={`/recipes/${recipe.sys.id}`}>Show Details</Link> */}
-                  </div>
-                ))}
-              </div>
-            </div>
+            <h2>{dish.fields.recipe1}</h2>
+            <img src={dish.fields.recipe1Media.fields.file.url}></img>
+            <p>{dish.fields.ingridients}</p>
+            <RichText document={dish.fields.directions} />
           </>
+          }
+                </div>
         );
       };
 // }
